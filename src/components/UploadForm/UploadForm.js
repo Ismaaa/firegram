79d54289497c1e2b2aa89e66b233ad5dd039abf8
Isma <didouhismael@gmail.com>
2020-08-02 22:22:59 +0200

    init form with state

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const ALLOWED_TYPES = ['image/png', 'image/jpeg'];
 
 const UploadForm = () => {
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState(null);
+
+  const onChangeHandler = (event) => {
+    const uploadedFile = event.target.files[0];
+
+    if (uploadedFile && ALLOWED_TYPES.includes(uploadedFile.type)) {
+      setImage(uploadedFile);
+      setError('');
+    } else {
+      setImage(null);
+      setError('Please select a valid image file. PNG or JPEG');
+    }
+  };
+
   return (
     <form action="#">
-      <input type="file" />
+      <input type="file" onChange={onChangeHandler} />
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {image && image.name}
+      </div>
     </form>
   );
 };
